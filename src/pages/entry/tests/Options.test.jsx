099481e408e12a4863cb28c58b6d300b801d4cfb
ddobi <qhdqhdekd261@naver.com>
2024-05-08 13:01:12 +0900

    Optoins 컴포넌트에 대한 테스트를 추가한다
    스쿱에 유효하지 않은 값이 입력되면 소계를 업데이트하지 않는 테스트를 추가한다.

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,5 @@
 // import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import Options from "../Options";
 
@@ -32,4 +33,20 @@ test("displays image for each topping option from server", async () => {
     "M&Ms topping",
     "Hot fudge topping",
   ]);
+});
+
+test("스쿱에 유효하지 않은 숫자가 입력됐을 때 스쿱의 소계가 업데이트되지 않도록 한다", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType={"scoops"} />);
+
+  // 10보다 큰 값을 설정한다
+  const vanillaInput = await screen.findByRole("spinbutton");
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "20");
+
+  // 총합이 $0.00인지 확인
+  const scoopsTotal = screen.getByText("Scoops total: $", {
+    exact: false,
+  });
+  expect(scoopsTotal).toHaveTextContent("0.00");
 });
